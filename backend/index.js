936947e1a73a1578ab456c4b82ca9d33f7283619
my-suraj5656 <161513600+my-suraj5656@users.noreply.github.com>
2026-01-16 15:07:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,23 @@ dotenv.config();
 import express from "express";
 import "./worker/worker.js";
 import conn from "./conn/conn.js";
+import cors from "cors";
 import scheduleRoutes from "./route/scheduleRoute.js";
 import emailRoutes from "./route/emailRoute.js";
 
 const PORT = process.env.PORT || 3020;
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",           
+      "https://reachinbox-task.vercel.app", 
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // schedule
