a4c1c8b0315ca7dc1c9d541492029077d67b9f4b
my-suraj5656 <161513600+my-suraj5656@users.noreply.github.com>
2026-01-16 15:34:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",           
-      "https://reachinbox-task.vercel.app", 
+      "https://reach-inbox-task-xi.vercel.app/", 
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
